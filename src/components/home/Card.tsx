7c0eccbac21256da68575cceb851f42354e68229
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-10-08 09:12:46 +0530

    card border update

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -4,7 +4,7 @@ const Card = () => {
     return (
         <div className='flex flex-row flex-wrap -mx-3.5'>
             <div className="w-[20%] px-3.5">
-                <div className=""></div>
+                <div className="w-full h-full bg-white"></div>
             </div>
         </div>
     )
