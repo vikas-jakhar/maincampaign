ef802cc8c474f4232d5f59a75935623ba0ff3906
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-10-10 16:24:09 +0530

    chart update

--- src/components/home/Chart.tsx
+++ src/components/home/Chart.tsx
@@ -1,11 +1,11 @@
 "use client"
-import React, { useRef } from 'react';
-import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend, ChartOptions, ChartData, } from 'chart.js';
+import React from 'react';
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend, ChartOptions, ChartData, ChartArea } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+
 ChartJS.register(BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
 
 const Chart: React.FC = () => {
-    const chartRef = useRef<any>(null);
     const data: ChartData<'bar'> = {
         labels: ['Jun 2021', 'Jul 2021', 'Aug 2021', 'Sep 2021', 'Oct 2021', 'Nov 2021', 'Dec 2021', 'Jan 2022', 'Feb 2022', 'Mar 2022', 'Apr 2022', 'May 2022', 'Jun 2022'],
         datasets: [
@@ -21,7 +21,7 @@ const Chart: React.FC = () => {
             {
                 label: 'Revenue',
                 data: [15000, 10000, 12000, 20000, 19000, 17000, 20500, 10000, 15000, 9000, 13000, 18000, 30000],
-                backgroundColor: (context: { chart: { ctx: CanvasRenderingContext2D; chartArea: { top: number; bottom: number } } }) => {
+                backgroundColor: (context: { chart: { ctx: CanvasRenderingContext2D; chartArea: ChartArea } }) => {
                     const chart = context.chart;
                     const { ctx, chartArea } = chart;
                     if (!chartArea) {
@@ -36,12 +36,14 @@ const Chart: React.FC = () => {
             },
         ],
     };
-    const getGradient = (ctx: CanvasRenderingContext2D, chartArea: any) => {
+
+    const getGradient = (ctx: CanvasRenderingContext2D, chartArea: ChartArea) => {
         const gradient = ctx.createLinearGradient(0, chartArea.top, 0, chartArea.bottom);
         gradient.addColorStop(0, '#88FFBF');
         gradient.addColorStop(1, 'rgba(62, 254, 151, 0.12)');
         return gradient;
     };
+
     const options: ChartOptions<'bar'> = {
         maintainAspectRatio: false,
         responsive: true,
@@ -80,7 +82,7 @@ const Chart: React.FC = () => {
 
     return (
         <div className='max-w-[964px] w-full md:ml-auto h-[262px]'>
-            <Bar data={data} options={options} ref={chartRef} />
+            <Bar data={data} options={options} />
             <div className="flex items-center justify-between px-3 md:mt-5 mt-2">
                 {[...Array(13)].map((_, idx) => (
                     <div key={idx} className="flex flex-col items-center">
