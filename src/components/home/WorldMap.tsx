19dab1c457653f2fa1a95a21f82467a4f9873e1f
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-10-10 16:27:03 +0530

    map update

--- src/components/home/WorldMap.tsx
+++ src/components/home/WorldMap.tsx
@@ -1,47 +1,9 @@
-"use client";
-import React, { useEffect } from 'react';
-import { Root } from '@amcharts/amcharts5';
-import { MapChart, MapPolygonSeries } from '@amcharts/amcharts5/map';
-import * as am5 from '@amcharts/amcharts5';
-import * as am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
-import worldIndiaLow from '@amcharts/amcharts5-geodata/worldIndiaLow';
+import React from 'react'
 
-const WorldMap: React.FC = () => {
-  useEffect(() => {
-    // Create root and chart
-    const root = Root.new("chartdiv");
-
-    // Set themes
-    root.setThemes([am5themes_Animated]);
-
-    const chart = root.container.children.push(
-      MapChart.new(root, {
-        panX: "rotateX",
-        projection: am5map.geoNaturalEarth1()
-      })
-    );
-
-    const polygonSeries = chart.series.push(
-      MapPolygonSeries.new(root, {
-        geoJSON: worldIndiaLow,
-        exclude: ["AQ"] // Exclude Antarctica or any other unwanted regions
-      })
-    );
-    polygonSeries.mapPolygons.template.setAll({
-      tooltipText: "{name}",
-      interactive: true
-    });
-    polygonSeries.mapPolygons.template.states.create("hover", {
-      fill: am5.color(0x677935)
-    });
-    return () => {
-      root.dispose();
-    };
-  }, []);
-
+const WorldMap = () => {
   return (
-    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
-  );
-};
+    <div>WorldMap</div>
+  )
+}
 
-export default WorldMap;
+export default WorldMap
